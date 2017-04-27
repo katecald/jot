e923fe2793c98de03e3c7ece3e1f6b00d8bfe74d
Kate Caldwell <kate.kec.caldwell@gmail.com>
2017-04-27 15:23:09 -0400

    what are these changes?

--- index.ios.js
+++ index.ios.js
@@ -1,20 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import { Route, NativeRouter, Link } from 'react-router-native';
-
 
 import Navbar from './components/Navbar.js'
 import Welcome from './components/Welcome.js'
 import Today from './components/Today.js'
 import Notebook from './components/Notebook.js'
 import Partners from './components/Partners.js'
-
 
 const App = () => (
   <NativeRouter>
